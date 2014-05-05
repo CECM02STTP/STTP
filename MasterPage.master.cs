943e28226b6d10d7d7e2977430fa7cadf28b1997
g80002 <g80002@yahoo.com.tw>
2014-05-05 20:45:42 +0800

    基本login功能
    基本登入功能，帳號資料為 MemberData表單

---
+++ MasterPage.master.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+public partial class bootsshop_MasterPage : System.Web.UI.MasterPage
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        
+        if (Request.Cookies["name"] == null)
+        {
+            logout.Visible = false;
+        }
+        else {
+            UserName.Text = Request.Cookies["name"].Value + ", 歡迎";
+            login.Visible = false;
+        }
+    }
+    String strConn = ConfigurationManager.ConnectionStrings["STTPConnectionString"].ConnectionString;
+    protected void ButtonLogin_Click(object sender, EventArgs e)
+    {
+        using (SqlConnection Conn = new SqlConnection(strConn))
+        {
+            String strCmd = "select count(*) from MemberData where MemberID = @MemberID and PassWord = @PassWord";
+            using (SqlCommand cmd = new SqlCommand(strCmd, Conn))
+            {
+                cmd.Parameters.AddWithValue("@MemberID", MemberID.Text);
+                cmd.Parameters.AddWithValue("@PassWord", inputPassword.Text);
+                Conn.Open();
+                if (Convert.ToInt32(cmd.ExecuteScalar()) > 0)
+                {
+                    //Page.ClientScript.RegisterStartupScript(this.GetType(), "", "alert('是會員')", true);
+                    Response.Cookies["name"].Value = MemberID.Text;
+                    if (RememberChkBox.Checked)
+                    {
+                        Response.Cookies["name"].Expires = DateTime.Now.AddDays(1);//記住一天
+                    }
+                    UserName.Text = Response.Cookies["name"].Value +", 歡迎" ;
+                    logout.Visible = true;
+                    login.Visible = false;
+                    
+                }
+                else
+                {
+                    Page.ClientScript.RegisterStartupScript(this.GetType(), "", "alert('不是會員')", true);
+                }
+                Conn.Close();
+                
+            }
+            
+        }
+    }
+
+    protected void ButtonLogout_Click(object sender, EventArgs e)
+    {
+        Response.Cookies["name"].Expires = DateTime.Now.AddDays(-1);
+        UserName.Text = "";
+        logout.Visible = false;
+        login.Visible = true;
+    }
+}
